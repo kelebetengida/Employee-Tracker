3e0d75c82b920687bafb1621567846cdd0f87fca
Kelebet Engida <engida398@gmail.com>
2021-08-05 14:25:46 -0700

    The views all are working with a table module added and the add department section is complete.

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 const inquirer= require("inquirer");
-const express = require('express');
+const express = require("express");
 const mysql=require("mysql2");
-const PORT = process.env.PORT || 3306;
+const consoleT=require("console.table");
+const PORT = process.env.PORT || 3001;
 const app = express();
 const db = mysql.createConnection(
     {
       host: 'localhost',
-      PORT: 3306,
+      PORT: 3001,
       // MySQL username,
       user: 'root',
       // MySQL password
@@ -15,13 +16,18 @@ const db = mysql.createConnection(
     },
     console.log(`Connected to the employees database.`)
   );
+
+db.connect((error)=>{
+    if (error) throw error;
+    promptUser();
+})
 const promptUser=()=>{
     inquirer.prompt([
         
         {
             name: "options",
             type:"list",
-            message:"Select the next options please:",
+            message:"Select from the next options please:",
             choices:[
                 "view all departments",
                 "view all roles",
@@ -46,7 +52,7 @@ const promptUser=()=>{
                 case "add a department":
                     return addDepartment();
                 case "add a role":
-                    return  c();
+                    return  addRole();
                 case "add an employee":
                     return addEmployee();
                 case "update an employee role":
@@ -62,21 +68,24 @@ const promptUser=()=>{
   
 const viewAllDepartments=()=>{
     db.query('SELECT departments.id, departments.departments_name FROM departments', function (err, results) {
-        console.log(results);
-    })
+       console.table(results);
+    });
+    promptUser();
 };
 
 const viewAllRoles=()=>{
-    db.query('SELECT roles.id, roles.title, departments.departments_name FROM rolesINNER JOIN departmentsON roles.department_id = departments.id', function (err, results){
-        console.log(results);
-    })
-    
+    db.query('SELECT roles.id, roles.title, departments.departments_name FROM roles INNER JOIN departments ON roles.department_id = departments.id', function (err, results){
+        console.table(results);
+    });
+    promptUser();
 }
 
 const viewAllEmployee=()=>{
-    db.query('SELECT employees.id, employees.first_name, employees.last_name, roles.title,departments.departments_name,roles.salary FROM employees, roles, departmentsWHERE departments.id = roles.department_id AND roles.id = employees.role_idORDER BY employees.id ASC', function (err, results) {
-        console.log(results);
+    let sql= 'SELECT employees.id, employees.first_name, employees.last_name, roles.title, departments.departments_name, roles.salary FROM employees, roles, departments WHERE departments.id = roles.department_id AND roles.id = employees.role_id ORDER BY employees.id ASC'
+    db.query(sql, function (err, results) {
+        console.table(results);
     })
+    promptUser();
 }
 
 //add
@@ -88,23 +97,66 @@ const addDepartment=()=>{
                 type: "input",
                 name: "firstName",
                 message:"What is the name of the department?"
+               
             }
 
         ]
-    )
-    .then((answer)=>{
-        let sql = `INSERT INTO departments(departments_name)VALUES (?)`;
+        ).then((answer)=>{
+        let sql = `INSERT INTO departments (departments_name) VALUES ("${answer.firstName}")` 
         db.query(sql, answer.firstName, function (err,results){
             console.log(results);
+            viewAllDepartments();
         })
     })
+    
+    promptUser();
 }
 
 const addRole=()=>{
-    let sql ='SELECT * FROM departments';
+    
+    let arr=[];
+    let sql ='SELECT departments.id,departments_name FROM departments ORDER BY departments_name ASC';
     db.query(sql, function(err,results){
         console.log(results);
-    });
+    }).then((departments)=>{
+        for(i=0;i<departments.length;i++){
+            arr.push(departments[i].name);
+        }
+        return departments;
+    }).then((departments)=>{
+        inquirer.prompt(
+            [
+                {
+                    type: "input",
+                    name: "roleT",
+                    message:"What is the title of the role?"
+                },
+                {
+                    type: "input",
+                    name: "roleS",
+                    message:"What is the salary of the role?"
+                },
+                {
+                    type: "list",
+                    name: "departments",
+                    message:"Departments",
+                    choices: arr
+                }
+    
+            ]
+        ).then((answer)=>{
+            let departmentid;
+            for(i=0;i<departments.length;i++){
+                departmentid=departments[i].id;
+            }
+            let sql = `INSERT INTO roles (title, salary, department_id) VALUES ("${answer.roleT}", "${answer.roleS}", "${departmentid})`;
+            db.query(sql, answer.firstName, function (err,results){
+                console.log(results);
+            })
+            promptUser();
+
+        })
+    })
 
 
 
@@ -112,20 +164,39 @@ const addRole=()=>{
         [
             {
                 type: "input",
-                name: "firstName",
-                message:"What is the name of the department?"
+                name: "roleT",
+                message:"What is the title of the role?"
+            },
+            {
+                type: "input",
+                name: "roleS",
+                message:"What is the salary of the role?"
+            },
+            {
+                type: "list",
+                name: "departments",
+                message:"Departments",
+                choices: arr
             }
 
         ]
-    )
-    .then((answer)=>{
+    ).then((answer)=>{
         let sql = `INSERT INTO departments(departments_name)VALUES (?)`;
-        db.query(sql, answer.firstName, function (err,results){
+        db.query(sql, function (err,results){
             console.log(results);
         })
     })
+    promptUser();
 };
+ 
 
+const updateEmployeeRole=()=>{
+
+    let employeesA=[];
+    let roleA=[];
+
+    
+}
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
